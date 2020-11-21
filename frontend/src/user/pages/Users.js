6b303b44028005629d931f84522d640e7a6fe385
Tasikul Islam <iamtasikul@gmail.com>
2020-11-21 09:13:46 +0530

    Adding UsersList and UserItem Component

--- frontend/src/user/pages/Users.js
+++ frontend/src/user/pages/Users.js
@@ -1,5 +1,16 @@
+import React from "react";
+import UsersList from "../components/UsersList";
 const Users = () => {
-  return <h2>Users Page</h2>;
+  const USERS = [
+    {
+      id: "u1",
+      name: "Tasikul islam",
+      image:
+        "https://pbs.twimg.com/profile_images/1037678868276989958/i9xEAxFX.jpg",
+      places: 3,
+    },
+  ];
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
